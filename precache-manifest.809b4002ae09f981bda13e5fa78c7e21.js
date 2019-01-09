c502f9a5e091939355cc5db3d22deffd8519f01c
Corey Petty <corey@status.im>
2019-01-09 09:55:17 -0500

    changed to donations.secureth.org

---
+++ precache-manifest.809b4002ae09f981bda13e5fa78c7e21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bab8dba304a2325cb473",
+    "url": "/static/js/main.bab8dba3.chunk.js"
+  },
+  {
+    "revision": "cd24c01e20edd3e263d0",
+    "url": "/static/js/1.cd24c01e.chunk.js"
+  },
+  {
+    "revision": "bab8dba304a2325cb473",
+    "url": "/static/css/main.c4ea999e.chunk.css"
+  },
+  {
+    "revision": "e65926377c48f1f640a4cbc6021e1422",
+    "url": "/index.html"
+  }
+];
